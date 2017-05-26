b97f7df14b4528418ebb6d8bf3967545f4ca428a
emancipatedMind <powensfinch@gmail.com>
2017-05-26 13:45:47 -0400

    Added panel for game, disabled resize, and menu bar at with option to exit application.

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -12,16 +12,20 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TicTacToe
-{
+namespace TicTacToe {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
             InitializeComponent();
         }
+
+        private void Close_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
     }
+
 }
