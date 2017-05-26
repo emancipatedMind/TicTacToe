ac9eb34e7a2b2cde191cd49213b492ffb131bf79
emancipatedMind <powensfinch@gmail.com>
2017-05-26 18:13:59 -0400

    Added new reference to Tester project to use System.Windows.Controls.Button class. Added TearDown, and SetUp methods as well.

---
+++ TicTacToeTester/ComputerTester.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToeTester {
+    using NUnit.Framework;
+    using TicTacToe;
+    using System.Windows.Controls;
+    [TestFixture]
+    public class ComputerTester {
+
+        LocationCollection _collection = new LocationCollection();
+
+        [SetUp]
+        public void SetUpMethod() {
+            for (int i = 0; i < 9; i++) _collection.Add(new Location(new Button(), i));
+        }
+
+        [TearDown]
+        public void TearDownMethod() {
+            _collection.Reset();
+        }
+
+    }
+}
