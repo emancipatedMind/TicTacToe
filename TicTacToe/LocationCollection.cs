596cd879fa4d3924a1cce66aae0eae97e7dd3fd0
emancipatedMind <powensfinch@gmail.com>
2017-05-26 13:33:48 -0400

    Added class to serve as collection wrapper for Location class.

---
+++ TicTacToe/LocationCollection.cs
@@ -0,0 +1,22 @@
+﻿namespace TicTacToe {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Controls;
+    public class LocationCollection : IEnumerable<Location> {
+        List<Location> _collection = new List<Location>();
+
+        public void Add(Location l) => _collection.Add(l);
+
+        public IEnumerator<Location> GetEnumerator() =>
+            _collection.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            _collection.GetEnumerator();
+
+        public Location GetLocation(Button b) =>
+            _collection.Where(l => l.Button == b).First();
+
+        public void Reset() => _collection.ForEach(l => l.Reset());
+    }
+}
