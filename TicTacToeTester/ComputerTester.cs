160d54e550e2d8e5aa76184e12df2a99eb4783ca
emancipatedMind <powensfinch@gmail.com>
2017-05-26 20:36:54 -0400

    Decided to change logic. Removed Test.

--- TicTacToeTester/ComputerTester.cs
+++ TicTacToeTester/ComputerTester.cs
@@ -2,7 +2,6 @@
     using NUnit.Framework;
     using System.Windows.Controls;
     using TicTacToe;
-    using TicTacToe.AI;
     [TestFixture]
     public class ComputerTester {
 
@@ -16,25 +15,6 @@
         [TearDown]
         public void TearDownMethod() {
             _collection.Reset();
-        }
-
-        [Test]
-        [Apartment(System.Threading.ApartmentState.STA)]
-        public void Move_PlayerDoesNotSelectPosition4AsFirstMove_ComputerShouldSelectPosition4() {
-            for (int i = 0; i < 9; i++) {
-                if (i == 4) continue;
-                var computer = new Computer(_collection);
-                int indexSelectedByPlayer = i;
-                Location ln = _collection.GetLocation(indexSelectedByPlayer);
-                ln.State = State.X;
-                computer.Move();
-                for (int j = 0; j < 9; j++) {
-                    if (indexSelectedByPlayer == j) continue;
-                    State expectedState = j == 4 ? State.O : State.Open;
-                    Assert.AreEqual(expectedState, _collection.GetLocation(j).State);
-                }
-                _collection.Reset();
-            }
         }
 
     }
