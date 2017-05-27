d819e2d7b23163812052d8a7b371dd18684e5b43
emancipatedMind <powensfinch@gmail.com>
2017-05-26 20:01:22 -0400

    Changed test to test, if foe does not take middle, computer will take middle.

--- TicTacToeTester/ComputerTester.cs
+++ TicTacToeTester/ComputerTester.cs
@@ -20,17 +20,22 @@
 
         [Test]
         [Apartment(System.Threading.ApartmentState.STA)]
-        public void Move_PlayerSelectsPosition0AsFirstMove_ComputerShouldSelectPosition4() {
-            var computer = new Computer(_collection);
-            int indexSelectedByPlayer = 0;
-            Location ln = _collection.GetLocation(indexSelectedByPlayer);
-            ln.State = State.X;
-            computer.Move();
+        public void Move_PlayerDoesNotSelectPosition4AsFirstMove_ComputerShouldSelectPosition4() {
             for (int i = 0; i < 9; i++) {
-                if (indexSelectedByPlayer == i) continue;
-                State expectedState = i == 4 ? State.O : State.Open;
-                Assert.AreEqual(expectedState, _collection.GetLocation(i).State);
+                if (i == 4) continue;
+                var computer = new Computer(_collection);
+                int indexSelectedByPlayer = i;
+                Location ln = _collection.GetLocation(indexSelectedByPlayer);
+                ln.State = State.X;
+                computer.Move();
+                for (int j = 0; j < 9; j++) {
+                    if (indexSelectedByPlayer == j) continue;
+                    State expectedState = j == 4 ? State.O : State.Open;
+                    Assert.AreEqual(expectedState, _collection.GetLocation(j).State);
+                }
+                _collection.Reset();
             }
         }
+
     }
 }
