8867bda16a13fbb9b05e8fa7cbc0a12d45736363
emancipatedMind <powensfinch@gmail.com>
2017-05-31 20:22:26 -0400

    Added first test.

--- TicTacToe/GameLogic.cs
+++ TicTacToe/GameLogic.cs
@@ -1,4 +1,12 @@
 ﻿namespace TicTacToe {
+    using System;
     public class GameLogic {
+
+        public event EventHandler MoveFound;
+
+        public void PlayRound() {
+            MoveFound?.Invoke(this, EventArgs.Empty);
+        }
+
     }
 }
--- TicTacToeTester/GameLogicTester.cs
+++ TicTacToeTester/GameLogicTester.cs
@@ -11,5 +11,14 @@
         public void TearDownMethod() {
         }
 
+        [Test]
+        public void PlayRound_MethodIsRan_EventFiredIsMoveFound() {
+            var gm = new TicTacToe.GameLogic();
+            bool eventFound = false;
+            gm.MoveFound += (s, e) => eventFound = true;
+            gm.PlayRound();
+            Assert.IsTrue(eventFound);
+        }
+
     }
 }
