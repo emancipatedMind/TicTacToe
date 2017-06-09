9a22316c8e959b56269c3c94b58323d6c2592cf9
emancipatedMind <powensfinch@gmail.com>
2017-06-08 22:10:31 -0400

    Added object overrides, and some operator overrides to Position class to help with comparison.

--- TicTacToe/Position.cs
+++ TicTacToe/Position.cs
@@ -7,7 +7,22 @@
         public Position(int column, int row) : this() {
             Column = column;
             Row = row;
-        } 
+        }
+
+        public override string ToString() =>
+            $"[{nameof(Column)},{Column};{nameof(Row)},{Row}]";
+
+        public override int GetHashCode() =>
+            ToString().GetHashCode();
+
+        public override bool Equals(object obj) =>
+            obj is Position && GetHashCode() == obj.GetHashCode();
+
+        public static bool operator ==(Position p1, Position p2) =>
+            p1.Row == p2.Row && p1.Column == p2.Column;
+
+        public static bool operator !=(Position p1, Position p2) =>
+            !(p1 == p2);
 
     }
 }
