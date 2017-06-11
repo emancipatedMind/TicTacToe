3c4d3af10c8cc5d5f9ffac1af3220596f0a1ba4c
emancipatedMind <powensfinch@gmail.com>
2017-06-11 10:20:36 -0400

    Added mock to use for testing.

---
+++ TicTacToeTester/GameContextMock.cs
@@ -0,0 +1,7 @@
+﻿namespace TicTacToeTester {
+    using TicTacToe;
+    public class GameContextMock : IGameContext {
+        public MoveCollection Moves { get; } = new MoveCollection();
+        public IGameState State { get; set; }
+    }
+}
