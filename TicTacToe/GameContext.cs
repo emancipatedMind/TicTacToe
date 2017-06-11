15f8051be1b783d8785ee017139e8fb4bc8c2c41
emancipatedMind <powensfinch@gmail.com>
2017-06-11 00:33:53 -0400

    Moved logic that initializes MoveCollection for the game to GameContext. Seems more appropriate.

--- TicTacToe/GameContext.cs
+++ TicTacToe/GameContext.cs
@@ -1,5 +1,12 @@
 ﻿namespace TicTacToe {
     public class GameContext : IGameContext {
+
+        public GameContext() {
+            for (int column = 0; column < 3; column++)
+                for (int row = 0; row < 3; row++)
+                    Moves.Add(new Move(new Position(column, row)));
+        }
+
         public MoveCollection Moves { get; } = new MoveCollection();
         public IGameState State { get; set; }
     }
--- TicTacToe/MoveCollection.cs
+++ TicTacToe/MoveCollection.cs
@@ -1,26 +1,16 @@
 ﻿namespace TicTacToe {
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
     public class MoveCollection : IEnumerable<Move> {
 
         List<Move> _moves = new List<Move>();
 
-        public MoveCollection() {
-            for (int column = 0; column < 3; column++)
-                for (int row = 0; row < 3; row++)
-                    _moves.Add(new Move(new Position(column, row)));
-        }
-
-        public void Reset() {
-            _moves.ForEach(m => m.Player = PositionBelongsTo.NoOne);
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() =>
-            _moves.GetEnumerator();
-
-        public IEnumerator<Move> GetEnumerator() =>
-            _moves.GetEnumerator();
-
+        public void Add(Move moveToAdd) => _moves.Add(moveToAdd);
+        public void Reset() => _moves.ForEach(m => m.Player = PositionBelongsTo.NoOne);
+        IEnumerator IEnumerable.GetEnumerator() => _moves.GetEnumerator();
+        public IEnumerator<Move> GetEnumerator() => _moves.GetEnumerator();
         public Move this[int index] => _moves[index];
+        public Move this[Position position] => _moves.Where(m => m.Position == position).First();
     }
 }
