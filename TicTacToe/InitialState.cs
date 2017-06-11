2ba009b62d11b7b8e5532f73b5caf9302a79474f
emancipatedMind <powensfinch@gmail.com>
2017-06-11 12:41:35 -0400

    Added initial state, and a test for the first function of state.

---
+++ TicTacToe/InitialState.cs
@@ -0,0 +1,29 @@
+﻿namespace TicTacToe {
+    using System;
+    using System.Linq;
+    public class InitialState : IGameState {
+
+        public InitialState(IGameContext context) {
+            Context = context;
+            Context.State = this;
+        }
+
+        public IGameContext Context { get; private set; }
+
+        public event EventHandler<MoveFoundEventArgs> MoveFound;
+
+        public void Handle() {
+
+            var availableMoves = Context.Moves.Where(m => m.Player == PositionBelongsTo.NoOne);
+            var userMoves = Context.Moves.Where(m => m.Player == PositionBelongsTo.User);
+
+            var rnd = new Random();
+            Move[] currentlyAvailableMoves = availableMoves.ToArray();
+            Move selectedMove = currentlyAvailableMoves[rnd.Next(currentlyAvailableMoves.Length)]; 
+            selectedMove.Player = PositionBelongsTo.Computer;
+            MoveFound?.Invoke(this, new MoveFoundEventArgs(selectedMove.Position));
+
+        }
+
+    }
+}
---
+++ TicTacToeTester/InitialStateTester.cs
@@ -0,0 +1,31 @@
+﻿namespace TicTacToeTester {
+    using NUnit.Framework;
+    using TicTacToe;
+    [TestFixture] 
+    public class InitialStateTester {
+
+        [Test] 
+        public void Handle_MoveCollectionContainsNoMovesByEitherPlayer_MoveFoundEventFiresWithRandomMove() {
+            var contextStub = new GameContextMock();
+
+            for (int column = 0; column < 3; column++)
+                for (int row = 0; row < 3; row++)
+                    contextStub.Moves.Add(new Move(new Position(column, row))); 
+
+            Position position = new Position();
+            bool eventFired = false;
+
+            var initialState = new InitialState(contextStub);
+            initialState.MoveFound += (s, e) => {
+                eventFired = true;
+                position = e.Position;
+            };
+            initialState.Handle();
+
+            Assert.IsTrue(eventFired);
+            Assert.IsTrue(position.Column >= 0 && position.Column < 3);
+            Assert.IsTrue(position.Row >= 0 && position.Row < 3);
+        }
+    }
+
+}
