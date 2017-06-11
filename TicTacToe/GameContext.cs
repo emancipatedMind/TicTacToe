87579c19037dab3661bb35ee138ca5114d378533
emancipatedMind <powensfinch@gmail.com>
2017-06-11 00:00:23 -0400

    Created Context file for State Pattern implementation.

---
+++ TicTacToe/GameContext.cs
@@ -0,0 +1,6 @@
+﻿namespace TicTacToe {
+    public class GameContext : IGameContext {
+        public MoveCollection Moves { get; } = new MoveCollection();
+        public IGameState State { get; set; }
+    }
+}
