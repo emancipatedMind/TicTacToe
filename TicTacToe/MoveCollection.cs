16e03fd5c6729113fb1e9545e5c6030c30c0a0fa
emancipatedMind <powensfinch@gmail.com>
2017-06-10 21:19:19 -0400

    Added a MoveCollection to manage the legal moves of the game.

---
+++ TicTacToe/MoveCollection.cs
@@ -0,0 +1,26 @@
+﻿namespace TicTacToe {
+    using System.Collections;
+    using System.Collections.Generic;
+    public class MoveCollection : IEnumerable<Move> {
+
+        List<Move> _moves = new List<Move>();
+
+        public MoveCollection() {
+            for (int column = 0; column < 3; column++)
+                for (int row = 0; row < 3; row++)
+                    _moves.Add(new Move(new Position(column, row)));
+        }
+
+        public void Reset() {
+            _moves.ForEach(m => m.Player = PositionBelongsTo.NoOne);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            _moves.GetEnumerator();
+
+        public IEnumerator<Move> GetEnumerator() =>
+            _moves.GetEnumerator();
+
+        public Move this[int index] => _moves[index];
+    }
+}
