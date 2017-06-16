c645bb962da714316205fd4f5b82bebb6ff651d6
emancipatedMind <powensfinch@gmail.com>
2017-06-15 22:21:47 -0400

    Judge now contains method that determines whether computer has won game. The logic in the method that was used to determine if user won game has been abstracted out, and generalized for use by both methods.

--- TicTacToe/Judge.cs
+++ TicTacToe/Judge.cs
@@ -9,11 +9,17 @@
             _board = board;
         }
 
-        public bool HasDeterminedThatUserWonGameWith(Position latestPosition) {
-            Position[] userPositions = _board.Where(m => m.Player == PositionBelongsTo.User).Select(m => m.Position).ToArray();
+        public bool HasDeterminedThatUserWonGameWith(Position latestPosition) =>
+            CheckIfGameHasBeenWon(PositionBelongsTo.User, latestPosition);
+
+        public bool HasDeterminedThatComputerWonGameWith(Position latestPosition) =>
+            CheckIfGameHasBeenWon(PositionBelongsTo.Computer, latestPosition);
+
+        private bool CheckIfGameHasBeenWon(PositionBelongsTo player, Position latestPosition) {
+            Position[] positions = _board.Where(m => m.Player == player).Select(m => m.Position).ToArray();
             Position[][] possibleWinningPositions = _winningSetRetriever.GetWinningPositions(latestPosition);
             foreach(Position[] set in possibleWinningPositions) {
-                if (userPositions.Contains(set[0]) && userPositions.Contains(set[1]))
+                if (positions.Contains(set[0]) && positions.Contains(set[1]))
                     return true;
             }
             return false;
