48d759463fa11fa391a3589be673a06fa2648ad9
emancipatedMind <powensfinch@gmail.com>
2017-06-17 14:39:56 -0400

    Added new test to make sure judge recognizes when computer has won.

--- TicTacToeTester/JudgeTester.cs
+++ TicTacToeTester/JudgeTester.cs
@@ -57,6 +57,30 @@
                 var winningPosition = new Position(2, 1);
                 Assert.Throws<GameHasBeenWonException>(() => judge.ChecksToSeeIfUserEndedGameWith(winningPosition));
             }
+
+            [Test]
+            public void computer_submitted_winning_position() {
+                MoveCollection board = new MoveCollection();
+                var judge = new Judge(board);
+
+                for (int column = 0; column < 3; column++)
+                    for (int row = 0; row < 3; row++)
+                        board.Add(new Move(new Position(column, row)));
+
+                Move[] game = new Move[] {
+                    new Move(PositionBelongsTo.Computer, new Position(0, 1)),
+                    new Move(PositionBelongsTo.User, new Position(2, 0)),
+                    new Move(PositionBelongsTo.Computer, new Position(1, 1)),
+                    new Move(PositionBelongsTo.User, new Position(2, 1)),
+                    new Move(PositionBelongsTo.Computer, new Position(2, 1)),
+                };
+
+                foreach(var move in game)
+                    board[move.Position].Player = move.Player;
+
+                var winningPosition = new Position(2, 1);
+                Assert.Throws<GameHasBeenWonException>(() => judge.ChecksToSeeIfComputerEndedGameWith(winningPosition));
+            }
         }
 
         [TestFixture]
