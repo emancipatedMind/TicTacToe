6d435fa796270d5e8f6ce481452633a8c5dcbcd1
emancipatedMind <powensfinch@gmail.com>
2017-06-18 01:34:18 -0400

    Added new class to represent the EndGameState of the match.

---
+++ TicTacToe/EndGameState.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacToe {
+    using System;
+    public class EndGameState : IGameState {
+
+        public EndGameState(IGameState state) : this(state.Context) { }
+
+        public EndGameState(IGameContext context) {
+            Context = context;
+        }
+
+        public IGameContext Context { get; private set; }
+
+        public event EventHandler<MoveFoundEventArgs> MoveFound;
+
+        public void PlayRound() {
+        }
+
+    }
+}
