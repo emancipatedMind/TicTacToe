fb464b72d2b0cf6a99bf6297c42390eb77aa9d8e
emancipatedMind <powensfinch@gmail.com>
2017-06-18 03:37:14 -0400

    Split EndGame into two states. Two moves left, and One move left.

---
+++ TicTacToe/NoComputerMoveEndGameState.cs
@@ -0,0 +1,24 @@
+﻿namespace TicTacToe {
+    using System;
+    using System.Linq;
+    public class NoComputerMoveEndGameState : IGameState {
+
+        public NoComputerMoveEndGameState(IGameState state) : this(state.Context) { }
+
+        public NoComputerMoveEndGameState(IGameContext context) {
+            Context = context;
+        }
+
+        public IGameContext Context { get; private set; }
+
+        public event EventHandler<MoveFoundEventArgs> MoveFound;
+
+        public void PlayRound() {
+            Position usersLastMove = Context.MoveHistory.Last().Position;
+            Context.Judge.ChecksToSeeIfUserEndedGameWith(usersLastMove);
+
+            throw new GameHasEndedInTieException();
+        }
+
+    }
+}
---
+++ TicTacToe/TwoMovesLeftEndGameState.cs
@@ -0,0 +1,29 @@
+﻿namespace TicTacToe {
+    using System;
+    using System.Linq;
+    public class TwoMovesLeftEndGameState : IGameState {
+
+        public TwoMovesLeftEndGameState(IGameState state) : this(state.Context) { }
+
+        public TwoMovesLeftEndGameState(IGameContext context) {
+            Context = context;
+        }
+
+        public IGameContext Context { get; private set; }
+
+        public event EventHandler<MoveFoundEventArgs> MoveFound;
+
+        public void PlayRound() {
+            Position usersLastMove = Context.MoveHistory.Last().Position;
+            Context.Judge.ChecksToSeeIfUserEndedGameWith(usersLastMove);
+
+            Position computerChosenPosition = Context.ComputerPlayer.MakeMove();
+            Context.Board[computerChosenPosition].Player = PositionBelongsTo.Computer;
+            MoveFound?.Invoke(this, new MoveFoundEventArgs(computerChosenPosition));
+            Context.Judge.ChecksToSeeIfComputerEndedGameWith(computerChosenPosition);
+
+            throw new GameHasEndedInTieException();
+        }
+
+    }
+}
