ce70a72b77622a14f881963a8ad8994479799ab6
emancipatedMind <powensfinch@gmail.com>
2017-06-18 01:43:33 -0400

    GameContextMock should not have a private setter.

--- TicTacToeTester/GameContextMock.cs
+++ TicTacToeTester/GameContextMock.cs
@@ -3,7 +3,7 @@
     public class GameContextMock : IGameContext {
         public MoveCollection Board { get; } = new MoveCollection();
         public IGameState State { get; set; }
-        public IMoveMaker ComputerPlayer { get; private set; }
+        public IMoveMaker ComputerPlayer { get; set; }
         public Judge Judge { get; private set; }
     }
 }
