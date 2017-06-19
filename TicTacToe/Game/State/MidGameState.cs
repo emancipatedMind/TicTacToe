45db3b83d4a1dc0ae367341bc95203faa1bf7fc0
emancipatedMind <powensfinch@gmail.com>
2017-06-19 19:26:30 -0400

    Added logic to the MidGameState.

--- TicTacToe/Game/State/MidGameState.cs
+++ TicTacToe/Game/State/MidGameState.cs
@@ -1,5 +1,6 @@
 ﻿namespace TicTacToe.Game.State {
     using System;
+    using System.Linq;
     public class MidGameState : IGameState {
 
         public MidGameState(IGameState state) : this(state.Context) { }
@@ -13,6 +14,33 @@
         public event EventHandler<MoveFoundEventArgs> MoveFound;
 
         public void PlayRound() {
+            Position usersLastMove = Context.MoveHistory.Last().Position;
+            Context.Judge.ChecksToSeeIfUserEndedGameWith(usersLastMove);
+
+            Position computerChosenPosition = Context.ComputerPlayer.MakeMove();
+            Context.Board[computerChosenPosition].Player = PositionBelongsTo.Computer;
+            MoveFound?.Invoke(this, new MoveFoundEventArgs(computerChosenPosition));
+            Context.Judge.ChecksToSeeIfComputerEndedGameWith(computerChosenPosition);
+
+            if (TwoMovesLeft)
+                Context.State = new TwoMovesLeftEndGameState(this);
+            else if (OnlyOneMoveLeft)
+                Context.State = new NoComputerMoveEndGameState(this);
+
+        }
+
+        private bool TwoMovesLeft {
+            get {
+                int availableMoveCount = Context.Board.Where(m => m.Player == PositionBelongsTo.NoOne).Count();
+                return availableMoveCount == 2;
+            }
+        }
+
+        private bool OnlyOneMoveLeft {
+            get {
+                int availableMoveCount = Context.Board.Where(m => m.Player == PositionBelongsTo.NoOne).Count();
+                return availableMoveCount == 1;
+            }
         }
 
     }
