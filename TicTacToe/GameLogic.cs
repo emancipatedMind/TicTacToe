4e16ee88b24dbf84fccb4cc592460091789c2f3a
emancipatedMind <powensfinch@gmail.com>
2017-06-21 18:37:14 -0400

    GameLogic will serve as my ViewModel/Controller. It now carries GameContext as a model, and has two Commands. StartRound is for when the computer goes first, and PlayRound is for all other plays. It also has two events: GameHasEndedInTie, and GameHasBeenWon. These are directly connected to the model's events.

--- TicTacToe/GameLogic.cs
+++ TicTacToe/GameLogic.cs
@@ -1,4 +1,26 @@
 ﻿namespace TicTacToe {
+    using Game;
+    using System;
+    using ToolkitNFW4.XAML;
     public class GameLogic {
+
+        GameContext _model = new GameContext();
+        DelegateCommand _playRoundCommand;
+        DelegateCommand _startRoundCommand;
+
+        public GameLogic() {
+            _model.GameHasEndedInTie += (s, e) => GameHasEndedInTie?.Invoke(this, e);
+            _model.GameHasBeenWon += (s, e) => GameHasBeenWon?.Invoke(this, e);
+        }
+
+        public event EventHandler GameHasEndedInTie;
+        public event EventHandler<GameHasBeenWonEventArgs> GameHasBeenWon;
+
+        public DelegateCommand PlayRound => _playRoundCommand ??
+            (_playRoundCommand = new DelegateCommand(o => _model.PlayRound((Position)o)));
+
+        public DelegateCommand StartRound => _startRoundCommand ??
+            (_startRoundCommand = new DelegateCommand(o => _model.PlayRound()));
+
     }
 }
