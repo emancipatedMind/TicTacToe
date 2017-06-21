44a0252fab8762f1f822a07e4316b25f9b2f28db
emancipatedMind <powensfinch@gmail.com>
2017-06-21 19:05:09 -0400

    Added class to serve as Controller/ViewModel called ButtonViewModel for the buttons to play the game.

---
+++ TicTacToe/ButtonViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TicTacToe {
+    using ToolkitNFW4.XAML;
+    public class ButtonViewModel : EntityBase {
+
+        Move _move;
+
+        public ButtonViewModel(Move move) {
+            _move = move;
+            _move.PlayerChanged += (s, e) =>
+                OnPropertyChanged(nameof(PositionOwner));
+        }
+
+        public Position Position => _move.Position;
+
+        public PositionBelongsTo PositionOwner => _move.Player;
+
+    }
+}
