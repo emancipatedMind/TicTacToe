29590dc5c68dc1786716a995917e603820a09723
emancipatedMind <powensfinch@gmail.com>
2017-06-22 13:52:39 -0400

    GameContext now contains all the logic necessary to play game.

--- TicTacToe/Game/GameContext.cs
+++ TicTacToe/Game/GameContext.cs
@@ -1,12 +1,86 @@
 ﻿namespace TicTacToe.Game {
     using Computer;
     using System;
+    using System.Linq;
     public class GameContext : IGameContext {
-        public MoveCollection Board => throw new NotImplementedException();
-        public IMoveMaker ComputerPlayer { get; set; }
+
+        #region Fields
+        WinningSetRetriever _winningSetRetriever = new WinningSetRetriever();
+        bool _gameOver = false;
+        #endregion
+
+        #region Constructors
+        public GameContext() {
+
+            for (int column = 0; column < 3; column++) {
+                for (int row = 0; row < 3; row++) {
+                    var move = new Move(new Position(column, row));
+                    move.PlayerChanged += Move_PlayerChanged;
+                    Board.Add(move);
+                }
+            }
+
+            ComputerPlayer = new ComputerPlayer(Board);
+        }
+        #endregion
+
+        #region Events
         public event EventHandler GameHasEndedInTie;
         public event EventHandler<GameHasBeenWonEventArgs> GameHasBeenWon;
-        public void PlayRound(Position position) { }
-        public void PlayRound() { }
+        #endregion
+
+        #region Properties
+        public MoveCollection Board { get; set; } = new MoveCollection();
+        public IMoveMaker ComputerPlayer { get; set; }
+        #endregion
+
+        #region Methods
+        public void PlayRound(Position position) => MakeMove(position);
+
+        public void PlayRound() => MakeMove();
+        #endregion
+
+        #region Support Methods
+        protected void MakeMove(Position? playerMove = null) {
+
+            if (playerMove != null)
+                Board[playerMove.Value].Player = PositionBelongsTo.User;
+
+            if (_gameOver)
+                return;
+
+            Position computerChosenPosition = ComputerPlayer.MakeMove();
+            Board[computerChosenPosition].Player = PositionBelongsTo.Computer;
+
+        }
+
+        protected void Move_PlayerChanged(object sender, PlayerChangedEventArgs e) {
+            var move = (Move)sender;
+
+            var playerPositionsQuery = Board.Where(m => m != move && m.Player == move.Player).Select(m => m.Position);
+            var availableMoves = Board.Where(m => m.Player == PositionBelongsTo.NoOne);
+
+            if (e.NewPlayer == PositionBelongsTo.NoOne || availableMoves.Count() >= 5)
+                return;
+
+            Position[] playerPositions = playerPositionsQuery.ToArray();
+            Position[][] possibleWinningPositions = _winningSetRetriever.GetWinningPositions(move.Position);
+
+            foreach (var set in possibleWinningPositions) {
+                if (playerPositions.Contains(set[0]) && playerPositions.Contains(set[1])) {
+                    GameHasBeenWon?.BeginInvoke(this, new GameHasBeenWonEventArgs(move), null, null);
+                    _gameOver = true;
+                    return;
+                }
+            }
+
+            if (availableMoves.Count() == 0) {
+                GameHasEndedInTie?.BeginInvoke(this, EventArgs.Empty, null, null);
+                _gameOver = true;
+            }
+
+        }
+        #endregion
+
     }
 }
