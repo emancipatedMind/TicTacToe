8cfc769e58215dc2c9adcff8ebaca519f480b973
emancipatedMind <powensfinch@gmail.com>
2017-06-21 20:42:26 -0400

    Added logic to fill out the board, and assign each created button its own DataContext contained inside of the list inside of the ViewModel. The events for a tie game, and for the game being won are being handled in a generic way for now. Each button has, as a command parameter, its position on the grid, and will feed this to the PlayRound command in order to communicate which button has been pressed by user.

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -18,6 +18,39 @@ namespace TicTacToe {
     /// </summary>
     public partial class MainWindow : Window {
 
+        public MainWindow() {
+            InitializeComponent();
+
+            var viewModel = (GameLogic) Application.Current.Resources["viewModel"];
+            var buttonList = new List<Button>();
+
+            for (int column = 0; column < 3; column++) {
+                for (int row = 0; row < 3; row++) {
+                    var button = new Button();
+                    Grid.SetColumn(button, column);
+                    Grid.SetRow(button, row);
+                    var position = new Position(column, row);
+                    button.SetBinding(DataContextProperty, new Binding { Source = viewModel.ButtonViewModelList.First(o => o.Position == position) });
+                    button.CommandParameter = position;
+                    button.Command = viewModel.PlayRound;
+                    board.Children.Add(button);
+                    buttonList.Add(button);
+                }
+            }
+
+            viewModel.GameHasBeenWon += (s, e) => {
+                board.Dispatcher.Invoke(new Action(() => {
+                    buttonList.ForEach(b => b.IsEnabled = false);
+                }));
+            };
+
+            viewModel.GameHasEndedInTie += (s, e) => {
+                board.Dispatcher.Invoke(new Action(() => {
+                    MessageBox.Show("Tie. Restart Application to play another game.");
+                }));
+            };
+        }
+
         private void Close_Click(object sender, RoutedEventArgs e) {
             Close();
         }
