43bb3f58b3f8c88ade9513fdf08bc4fd4feac57e
emancipatedMind <powensfinch@gmail.com>
2017-06-21 20:36:30 -0400

    Each button in the game will have its own DataContext, and this will be stored in a list inside of GameLogic. The board will be iterated over, and a ButtonViewModel will be created for each Position on the board.

--- TicTacToe/GameLogic.cs
+++ TicTacToe/GameLogic.cs
@@ -1,6 +1,7 @@
 ﻿namespace TicTacToe {
     using Game;
     using System;
+    using System.Collections.Generic;
     using ToolkitNFW4.XAML;
     public class GameLogic {
 
@@ -9,12 +10,17 @@
         DelegateCommand _startRoundCommand;
 
         public GameLogic() {
+            foreach (var b in _model.Board) {
+                ButtonViewModelList.Add(new ButtonViewModel(b));
+            }
             _model.GameHasEndedInTie += (s, e) => GameHasEndedInTie?.Invoke(this, e);
             _model.GameHasBeenWon += (s, e) => GameHasBeenWon?.Invoke(this, e);
         }
 
         public event EventHandler GameHasEndedInTie;
         public event EventHandler<GameHasBeenWonEventArgs> GameHasBeenWon;
+
+        public List<ButtonViewModel> ButtonViewModelList { get; } = new List<ButtonViewModel>();
 
         public DelegateCommand PlayRound => _playRoundCommand ??
             (_playRoundCommand = new DelegateCommand(o => _model.PlayRound((Position)o)));
