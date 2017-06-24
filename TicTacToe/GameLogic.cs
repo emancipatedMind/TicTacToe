432ee3ed3171f7b12e26d172a7a3a02526184f9f
emancipatedMind <powensfinch@gmail.com>
2017-06-24 13:25:21 -0400

    To GameLogic, added Property to expose GameContext.GameOver, and a command to allow a reset of GameContext. To the GameHasEndedInTie, and the GameHasBeenWon events of GameContext, GameLogic will fire PropertyChanged event for GameOver property before firing its own event.

--- TicTacToe/GameLogic.cs
+++ TicTacToe/GameLogic.cs
@@ -12,20 +12,34 @@
             foreach (var b in _model.Board) {
                 ButtonViewModelList.Add(new ButtonViewModel(b));
             }
-            _model.GameHasEndedInTie += (s, e) => GameHasEndedInTie?.Invoke(this, e);
-            _model.GameHasBeenWon += (s, e) => GameHasBeenWon?.Invoke(this, e);
+            _model.GameHasEndedInTie += (s, e) => {
+                OnPropertyChanged(nameof(GameOver));
+                GameHasEndedInTie?.Invoke(this, e);
+            };
+            _model.GameHasBeenWon += (s, e) => {
+                OnPropertyChanged(nameof(GameOver));
+                GameHasBeenWon?.Invoke(this, e);
+            };
             PlayRound = new DelegateCommand(o => _model.PlayRound((Position)o));
+            Reset = new DelegateCommand(ResetCallback);
         }
 
         public event EventHandler GameHasEndedInTie;
         public event EventHandler<GameHasBeenWonEventArgs> GameHasBeenWon;
 
         public List<ButtonViewModel> ButtonViewModelList { get; } = new List<ButtonViewModel>();
+        public bool GameOver => _model.GameOver;
 
         public DelegateCommand PlayRound { get; private set; }
+        public DelegateCommand Reset { get; private set; }
 
         public DelegateCommand StartRound => _startRoundCommand ??
             (_startRoundCommand = new DelegateCommand(o => _model.PlayRound()));
+
+        private void ResetCallback() {
+            _model.Reset();
+            OnPropertyChanged(nameof(GameOver));
+        }
 
     }
 }
